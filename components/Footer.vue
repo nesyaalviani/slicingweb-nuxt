7d2350e016a2dc11f1241299ede0eabc8b306b93
Nesya <nesya31120com@gmail.com>
2025-07-02 16:04:41 +0700

    coba

---
+++ components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+    <footer class="py-14 px-6 bg-gradient-to-b from-pink-400 to-purple-600 text-white ">
+    <div class="grid md:grid-cols-5 gap-8 text-sm">
+        <div>
+            <h3 class="font-bold text-lg mb-2">Etech.</h3>
+            <p>Platform belajar online yang modern untuk siswa abad ke-21.</p>
+        </div>
+        <div>
+            <h4 class="font-bold mb-2">Perusahaan</h4>
+        <ul>
+            <li><a href="#">Tentang Kami</a></li>
+            <li><a href="#">Karir</a></li>
+            <li><a href="#">Testimoni</a></li>
+        </ul>
+        </div>
+        <div>
+            <h4 class="font-bold mb-2">Bantuan</h4>
+        <ul>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">Bantuan Teknis</a></li>
+        </ul>
+        </div>
+        <div>
+        <h4 class="font-bold mb-2">Link</h4>
+        <ul>
+            <li><a href="#">Kursus</a></li>
+            <li><a href="#">Instruktur</a></li>
+        </ul>
+        </div>
+        <div>
+        <h4 class="font-bold mb-2">Kontak Kami</h4>
+        <p>+6281 1234 5678</p>
+        <p>etech@email.com</p>
+        </div>
+    </div>
+    <div class="text-center mt-10 text-purple-200 text-xs">
+        © 2025 Etech. Seluruh hak cipta dilindungi.
+    </div>
+</footer>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+module.exports = {
+  content: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
