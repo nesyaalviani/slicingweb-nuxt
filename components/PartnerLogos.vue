639126a5f563f1e9078784aa468e72345740d7ca
Nesya <nesya31120com@gmail.com>
2025-07-04 15:22:33 +0700

    c 2

--- components/PartnerLogos.vue
+++ components/PartnerLogos.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="bg-gradient-to-r from-purple-500 to-pink-500 py-6 px-4 sm:px-6 lg:px-8">
         <div class="max-w-7xl mx-auto flex justify-center items-center gap-6 sm:gap-10 flex-wrap">
-          <img src="../static/img/spotify-logo.png" alt="Duolingo" class="h-6 object-contain" />
+          <img src="../static/img/duolingo.svg" alt="Duolingo" class="h-6 object-contain" />
           <img src="img/codecov.svg" alt="Codecov" class="h-6 object-contain" />
           <img src="img/usertesting.svg" alt="UserTesting" class="h-6 object-contain" />
         </div>
